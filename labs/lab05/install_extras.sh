ba101128fd2041b3b556bcb82c52cf271a3b2960
oiopuppy <2069701323@qq.com>
2026-03-14 21:14:00 +0800

    fix: reorganize lab05 files to correct directory

---
+++ labs/lab05/install_extras.sh
@@ -0,0 +1,20 @@
+echo "begining download"
+sudo apt update
+sudo apt install -y \
+    dunst \
+    fonts-font-awesome \
+    fonts-powerline \
+    light \
+    fuzzel \
+    swaylock \
+    kitty \
+    waybar \
+    swaybg \
+    wl-clipboard \
+    mpv \
+    grim \
+    slurp
+
+sudo apt install -y fonts-iosevka
+
+echo "download finish"
